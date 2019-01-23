4e185de7b8ee1cd54a01c10874ea4925ca3b8923
Ken Morishita <mokemokechicken@gmail.com>
2019-01-23 10:44:03 +0900

    wip

--- spike/aws_container_manager.py
+++ spike/aws_container_manager.py
@@ -8,6 +8,8 @@ def main(container_id, entry_point):
     wi = WorkerInfo(container_id, entry_point)
     manager = AWSContainerManager(wi, "s3://bucket/")
     manager.login_container_registry()
+    manager.pull_container()
+    manager.run_container()
 
 
 if __name__ == '__main__':
--- spr_adbi/common/container.py
+++ spr_adbi/common/container.py
@@ -13,9 +13,17 @@ logger = getLogger(__name__)
 
 
 class ContainerManager:
-    def __init__(self, worker_info: WorkerInfo, base_uri: str):
+    def __init__(self, worker_info: WorkerInfo, base_uri: str, runtime_config=None):
+        """
+
+        :param worker_info:
+        :param base_uri:
+        :param runtime_config: kwargs of
+            https://github.com/docker/docker-py/blob/master/docker/models/containers.py#L506
+        """
         self.worker_info = worker_info
         self.base_uri = base_uri
+        self.runtime_config = runtime_config or {}
         self.setup()
 
     def setup(self):
@@ -30,7 +38,7 @@ class ContainerManager:
     def run_container(self):
         """
 
-        :return: (success:bool, stdout, stdin)
+        :return: (success:bool, stdout, stderr)
         """
         raise NotImplemented()
 
@@ -44,7 +52,6 @@ class AWSContainerManager(ContainerManager):
         super().setup()
         self.session = create_boto3_session_of_assume_role_delayed()
         self.ecr_client = self.session.client("ecr")
-        # print(self.ecr_client.meta.config)
 
     @property
     def region_name(self):
@@ -63,3 +70,16 @@ class AWSContainerManager(ContainerManager):
         registry_url = 'https://{account}.dkr.ecr.{region_name}.amazonaws.com/'.format(account=registry_ids[0],
                                                                                        region_name=self.region_name)
         self.docker_client.login(username=user_name, password=password, registry=registry_url)
+
+    def pull_container(self):
+        self.docker_client.images.pull(self.worker_info.container_id)
+
+    def run_container(self):
+        """
+
+        :return: (success:bool, stdout, stderr)
+        """
+        commands = self.worker_info.entry_point + [self.base_uri]
+        ret = self.docker_client.containers.run(self.worker_info.container_id, commands, stdout=True, stderr=True,
+                                                remove=True, **self.runtime_config)
+        print(ret)
