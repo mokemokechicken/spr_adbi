5f748962f3f908a6a74dcd8faed3f2d94f26afb9
Ken Morishita <mokemokechicken@gmail.com>
2019-01-28 14:58:43 +0900

    add tags to WorkerInfo

--- spr_adbi/dispatcher/resolver.py
+++ spr_adbi/dispatcher/resolver.py
@@ -5,11 +5,13 @@ class WorkerInfo:
     image_id: str
     entry_point: List[str]
     runtime_config: Optional[dict]
+    tags: List[str]
 
-    def __init__(self, image_id, entry_point, runtime_config=None):
+    def __init__(self, image_id, entry_point, runtime_config=None, tags=None):
         self.image_id = image_id
         self.entry_point = entry_point
         self.runtime_config = runtime_config
+        self.tags = tags or []
 
 
 class WorkerResolver:
